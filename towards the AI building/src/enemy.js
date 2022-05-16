c0fd3de505338277855a53d1ce574ed6ffe706ba
Dahye Kim <ekgp055@naver.com>
2022-05-16 16:39:41 +0900

    change folder name

---
+++ towards the AI building/src/enemy.js
@@ -0,0 +1,59 @@
+//벌 
+class Bee extends Phaser.GameObjects.Sprite {
+	constructor(config) {
+		super(config.scene, config.x, config.y, 'bee');
+		config.scene.add.existing(this);
+		config.scene.physics.add.existing(this);
+
+		this.x = config.x;
+		this.y = config.y;
+		this.side = 0;
+		// 벌은 위아래로 만 움직이도록 설정 
+		config.scene.tweens.add({
+			targets : this.body.velocity,
+			y : {from: -150, to: 150},
+			ease: Phaser.Math.Easing.Quadratic.InOut,
+			yoyo: true,
+			repeat: -1,
+			duration: 1000,
+			delay: Phaser.Math.Between(0,6) * 200
+		})
+	}
+
+	preUpdate() {
+		if (player.x < this.x) {
+			this.resetFlip();
+		}
+		if (player.x > this.x) {
+			this.flipX = true;
+		}
+	}
+}
+//슬라임 
+class Slime extends Phaser.GameObjects.Sprite {
+	constructor(config) {
+		super(config.scene, config.x, config.y, 'slime');
+		config.scene.add.existing(this);
+		config.scene.physics.add.existing(this);
+
+		this.x = config.x;
+		this.y = config.y;
+		this.dist = config.distance;
+		this.flipX = true;
+		this.speed = 1;
+		this.initial = [config.x, config.y];
+		this.body.allowGravity = false;
+	}
+
+	preUpdate() {
+		if (this.x > this.initial[0] + this.dist) {
+			this.speed *= -1;
+			this.toggleFlipX();
+		}
+		if (this.x < this.initial[0] - this.dist) {
+			this.speed *= -1;
+			this.toggleFlipX();
+		}
+		this.x += this.speed;
+	}
+}
---
+++ towards the AI building/src/script.js
@@ -0,0 +1,265 @@
+const WIDTH=864, HEIGHT=576;
+const TILESIZE = 48;
+const ROWS = HEIGHT / TILESIZE;
+const COLS = WIDTH / TILESIZE;
+const MAX_LEVEL = 6;
+
+var config = {
+	type: Phaser.AUTO,
+	width: WIDTH,
+	height: HEIGHT,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity : {y:300},
+			debug: false
+		}
+	},
+	scene: {
+		preload: preload,
+		create: create,
+		update:update
+	}
+}
+
+var game = new Phaser.Game(config);
+//처음 레벨 설정 
+var level = 2;
+var gameover = false;
+
+function preload() {
+	//this.load.image('bg', 'assets/BG1.png');
+	// assets 들 변수 지어주기 
+	this.load.image('bg', 'assets/BG2.png');
+	this.load.image('sun', 'assets/sun.png');
+	this.load.image('fire1', 'tiles/15.png');
+	this.load.image('fire2', 'tiles/16.png');
+	this.load.image('water1', 'tiles/19.png');
+	this.load.image('water2', 'tiles/20.png');
+	this.load.image('tree', 'tiles/21.png');
+	this.load.image('mushroom', 'tiles/22.png');
+	this.load.image('bee', 'tiles/23.png');
+	this.load.image('exit', 'tiles/24.png');
+	this.load.image('movingp', 'assets/moving.png');
+	this.load.image('flower', 'tiles/27.png');
+	this.load.image('slime', 'tiles/29.png');
+	this.load.spritesheet('dude' ,'assets/player.png', {
+		frameWidth: 32, frameHeight: 48
+	})
+	this.load.spritesheet('dude2','assets/pin.png',{
+		frameWidth: 28, frameHeight: 28
+	})
+	// tile들 불러오기 
+	for (let i=0; i<13; i++) {
+		// loading platforms
+		this.load.image(`tile${i+1}`, `tiles/${i+1}.png`);
+	}
+
+	for (let i=0; i<4; i++) {
+		// loading diamonds
+		this.load.image(`d${i+1}`, `assets/d${i+1}.png`);
+	}
+
+	this.load.json('levels', 'levels/level.json')
+}
+
+function create() {
+	// draw background
+	this.add.image(0, 0, 'bg').setOrigin(0).setScrollFactor(0);
+
+	//this.add.image(100, 120, 'sun');
+
+	// draw grid
+	// drawGrid(this);
+
+	// keypress Events
+	cursor = this.input.keyboard.createCursorKeys();
+
+	// create groups
+	// 정적그룹으로 설정 
+	platforms = this.physics.add.staticGroup();
+	diamonds = this.physics.add.staticGroup();
+	firewater = this.physics.add.staticGroup();
+	greens = this.physics.add.staticGroup();
+	exits = this.physics.add.staticGroup();
+	//버섯 기능추가 
+	mushrooms=this.physics.add.staticGroup();
+	bees = this.physics.add.group({allowGravity:false});
+	slimes = this.physics.add.group({allowGravity:false});
+	movingPlatforms = this.physics.add.group({allowGravity:false, immovable:true});
+	
+	// loading levels json 
+	// 여기서 json 에 저장해놓은거 가져옴 
+	var levels = this.cache.json.get('levels');
+	loadLevelSetup(levels, level, this);
+
+	// creating player
+	player = new Player({scene:this, x: 100, y:300});
+
+	// Collision Detection
+	this.physics.add.collider(player, platforms);
+	//water collision 의 경우  gameover 
+	this.physics.add.collider(player, firewater, gameOver, null, this);
+	//bees collision의 경우 gameover 
+	this.physics.add.collider(player, bees, gameOver, null, this);
+	//slimes collision의 경우 gameover 
+	this.physics.add.collider(player, slimes, gameOver, null, this);
+	//collisionMovingPlatform collision시 이동
+	this.physics.add.collider(player, movingPlatforms, collisionMovingPlatform, null, this);
+	//다이아몬드 overlap 경우 collectDiamonds 수행 
+	//overlap 과 collestion는 둘다 충돌해서 이벤트 발생하지만 
+	//overlap은 만남을 감지하고 어떤 작업을 수행 하는것이다 
+	this.physics.add.overlap(player, diamonds, collectDiamonds, null, this);
+	//버섯 닿으면 높게 올라가기
+	this.physics.add.overlap(player,mushrooms,mushroomUp,null,this);
+	// exits 문 접촉했을때 gamewon 
+	this.physics.add.overlap(player, exits, gameWon, null, this);
+
+	// Text Objects
+	levelText = this.add.text(WIDTH-200, 20, `Level : ${level}`, {fontSize:'32px', fill:'#000'})
+
+	// draw border
+	var rect = this.add.rectangle(0, 0, WIDTH, HEIGHT).setOrigin(0);
+	rect.setStrokeStyle(4, 0x1a65ac)
+}
+//캐릭터 움직임 
+function update() {
+	if (!gameover) {
+		//왼쪽 방향키
+		if (cursor.left.isDown) {
+			player.body.setVelocityX(-100);
+			player.anims.play('left', true);
+		} //오른쪽 방향기 
+		else if (cursor.right.isDown) {
+			player.body.setVelocityX(100);
+			player.anims.play('right', true);
+		}// 가만히 있기 
+		else {
+			player.body.setVelocityX(0);
+			player.anims.play('turn', true);
+		}
+		// 점프 
+		if (cursor.up.isDown && player.body.touching.down) {
+			player.body.setVelocityY(-400);
+		}
+
+		
+	}
+}
+// 레벨마다 맵 다르게 설정하는것 
+// 이 부분 코드 이해하면 맵 위치 설정 가능 
+//여기서 tiles 의 위치 설정 
+function loadLevelSetup(levels, level, scene) {
+	level_data = levels[level];
+	for (let i=0; i<level_data.length; i++) {
+		for (let j=0; j<level_data[0].length; j++) {
+			data = level_data[i][j];
+			[x, y] = getPos(i, j);
+
+			if (data && data <= 13) {
+				platforms.create(x, y,`tile${data}`).setScale(0.4).refreshBody();
+			}
+			//data =17 은 다이아몬드 
+			if (data && data == 17) {
+				diamonds.create(x, y,`d${randint(1, 4)}`).setScale(0.4).refreshBody();
+			}
+			//data =15 or data=16은 불 
+			if (data == 15 || data == 16) {
+				offy = data == 15 ? 10 : 0;
+				[x, y] = getPos(i, j, 0, offy);
+				firewater.create(x, y,`fire${data-14}`).setScale(0.4).refreshBody();
+			}
+			//data =19 or data=20은 물 
+			if (data == 19 || data == 20) {
+				offy = data == 19 ? 10 : 0;
+				[x, y] = getPos(i, j, 0, offy);
+				firewater.create(x, y,`water${data-18}`).setScale(0.4).refreshBody();
+			}
+			// data =21은 나무 
+			if (data == 21) {
+				greens.create(x, y-34, 'tree');
+			}
+			// data==2는 버섯
+			if (data == 22) {
+				greens.create(x, y+12, 'mushroom').refreshBody();
+			}
+			// data=23 벌
+			if (data == 23) {
+				var bee = new Bee({scene:scene, x: x, y:y})
+				bee.setScale(0.5);
+				bees.add(bee);
+			}
+			// data=24 탈출구 
+			if (data == 24) {
+				exits.create(x, y, 'exit');
+			}
+			// 25 는 왼쪽 오른쫌 움직임 ,26은 위아래 
+			if (data == 25 || data == 26) {
+				type = data == 25 ? 'side' : 'up';
+				var movingp = new MovingPlatform({scene:scene, x:x, y:y, key:'movingp', type:type})
+				movingPlatforms.add(movingp);
+			}
+
+			if (data == 27) {
+				var flower = greens.create(x, y+14, 'flower');
+				flower.setScale(0.2);
+			}
+
+			if (data == 29) {
+				var slime = new Slime({scene:scene, x: x, y:y+15, distance:50});
+				slime.setScale(0.5);
+				slimes.add(slime);
+			}
+		}
+	}
+}
+// 타일 position 잡는 함수 
+//TILESIZE =48 
+function getPos(i, j, offsetx=0, offsety=0) {
+	return [TILESIZE*(j+1)-24+offsetx, TILESIZE*(i+1)-24+offsety];
+}
+//다이아몬드 모으기 
+function collectDiamonds(player, diamond){
+	diamond.disableBody(true, true);
+}
+//게임 끝내기 
+function gameOver(player, tile) {
+	player.setTint(0xff0000);
+	this.physics.pause();
+	this.scene.restart();
+}
+// 레벨 올려주기 
+function gameWon(player, tile) {
+	if (level < MAX_LEVEL) {
+		level += 1;
+		this.scene.restart();
+	}
+}
+//움직이는 발판 
+function collisionMovingPlatform(player, platform) {
+	if (platform.body.touching.up && player.body.touching.down){
+		if (platform.body.y > player.body.y) {
+		    player.body.x += platform.vx;
+		    player.body.y += platform.vy;
+	    }
+	}
+}
+
+function drawGrid(scene) {
+	for (let i=0; i<ROWS; i++) {
+		scene.add.line(0, 0, 0, TILESIZE*i, WIDTH, TILESIZE*i, 0x1a65ac).setOrigin(0);
+	}
+	for (let j=0; j<COLS; j++) {
+		scene.add.line(0, 0, TILESIZE*j, 0, TILESIZE*j, HEIGHT, 0x1a65ac).setOrigin(0);
+	}
+}
+
+function randint(a, b) {
+	return Math.floor(Math.random() * b + a);
+}
+
+function mushroomUp(player,mushroom){
+	 
+	mushroom.disableBody(true, true);
+	 
+}
